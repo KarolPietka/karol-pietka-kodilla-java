692b55a1c512f8ad0fa65333290707814e19a6fc
KarolPietka <karolpietka164@gmail.com>
2019-06-12 21:23:19 +0200

    7.5

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,25 +1,28 @@
 package com.kodilla.stream.array;
 
-
+import java.util.NoSuchElementException;
+import java.util.OptionalDouble;
 import java.util.stream.IntStream;
-
 
 public interface ArrayOperations {
 
-    static double getAverage(int[] numbers) {
+
+    static double getAverage(int[] numbers) throws NoSuchElementException {
 
 
         IntStream.range(0, numbers.length)
-
                 .forEach(i -> System.out.println(numbers[i]));
 
+        OptionalDouble average = IntStream.of(numbers)
+                .average();
 
-        double average = IntStream.range(0, numbers.length)
-                .map(i -> numbers[i])
-                .average()
-                .getAsDouble();
-
-        return average;
+        if (average.isPresent()) {
+            return average.getAsDouble();
+        }else {
+            return 0;
+        }
     }
-
 }
+
+
+
