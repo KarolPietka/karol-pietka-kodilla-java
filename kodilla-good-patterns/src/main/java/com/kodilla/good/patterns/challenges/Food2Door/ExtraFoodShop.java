60327974f91e46f3e0d8c1a9add05b2b3637cd33
KarolPietka <karolpietka164@gmail.com>
2019-07-22 21:09:23 +0200

    14.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/ExtraFoodShop.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/ExtraFoodShop.java
@@ -1,11 +1,11 @@
 package com.kodilla.good.patterns.challenges.Food2Door;
 
 public class ExtraFoodShop implements Supplier {
-   public boolean process (OrderRequest orderRequest){
+    public boolean process(OrderRequest orderRequest) {
 
-       System.out.println("Customer: " + orderRequest.getCustomer() + "\n Ordered: " + orderRequest.getProduct() + "\n Supplier: " + orderRequest.getSupplier());
+        System.out.println("Customer: " + orderRequest.getCustomer() + "\n Ordered: " + orderRequest.getProduct() + "\n Supplier: " + orderRequest.getSupplier());
 
-       return true;
+        return true;
     }
-
 }
+
