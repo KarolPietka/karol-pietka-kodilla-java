91a7d170407b82661840b86140268e51d2aa6cdf
KarolPietka <karolpietka164@gmail.com>
2019-08-23 07:00:54 +0200

    17

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -0,0 +1,14 @@
+package com.kodilla.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class KodillaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (KodillaHibernateApplication.class, args);
+    }
+}
