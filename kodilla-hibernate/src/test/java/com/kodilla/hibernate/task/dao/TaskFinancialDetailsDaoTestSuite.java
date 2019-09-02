bc0645c19bc10387c7ba3a2a17dc9f1a7f8282eb
KarolPietka <karolpietka164@gmail.com>
2019-09-02 16:06:19 +0200

    17.4

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskFinancialDetailsDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskFinancialDetailsDaoTestSuite.java
@@ -28,7 +28,7 @@ public class TaskFinancialDetailsDaoTestSuite {
         List<TaskFinancialDetails> resultList = taskFinancialDetailsDao.findByPaid(false);
 
         //Then
-        Assert.assertEquals(5, resultList.size());
+        Assert.assertEquals(14, resultList.size());
 
         //CleanUp
         taskFinancialDetailsDao.deleteById(id);
